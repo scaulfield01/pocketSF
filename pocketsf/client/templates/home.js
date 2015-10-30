ae0370219280a80167ef52a998833c6b293e7e82
Steven Santa Maria <stevensm90@gmail.com>
2015-10-30 16:03:31 -0700

    add iron router + view + meteor frame

---
+++ pocketsf/client/templates/home.js
@@ -0,0 +1,11 @@
+Template.home.events({
+  'click .bike-link': function () {
+    // replace home template w bike template
+
+  }
+
+});
+
+Template.home.helpers({
+
+});
---
+++ pocketsf/lib/router.js
@@ -0,0 +1,7 @@
+Router.route('/', function () {
+  this.render('home');
+});
+
+Router.route('/bikes', function () {
+  this.render('bikes');
+});
