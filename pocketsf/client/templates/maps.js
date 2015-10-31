2b20195a221e430ddfb3b88f74a84947fa9b541e
suntorytime <stevensm90@gmail.com>
2015-10-31 16:22:44 -0700

    add info for markers

--- pocketsf/client/templates/maps.js
+++ pocketsf/client/templates/maps.js
@@ -28,13 +28,23 @@ Meteor.call('getBikeData', function(err,res){
 
     GoogleMaps.ready('exampleMap', function(map) {
 
-      for (var i = 0 ; i <  res.length;  i++) {
+      for (var i = 0 ; i <  res.length ;  i++) {
         var LatLng = new google.maps.LatLng(res[i].latitude, res[i].longitude)
+
+      var infowindow = new google.maps.InfoWindow({
+        content: "Name: " + res[i].name + " <br> Spaces: " + res[i].spaces
+       })
+
 
        var bikeRackMarkers = new google.maps.Marker({
           position: LatLng,
           map: map.instance
         });
+
+       bikeRackMarkers.addListener('click', function(){
+          infowindow.open(map.instance,  bikeRackMarkers);
+       })
+
       };
 
       var userMarker = new google.maps.Marker({
