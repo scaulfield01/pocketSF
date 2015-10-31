46b8126d46e134fc5ad23b962e99b2fd3c1aacef
suntorytime <stevensm90@gmail.com>
2015-10-31 14:15:47 -0700

    add second marker + server logic for marker

--- pocketsf/client/templates/maps.js
+++ pocketsf/client/templates/maps.js
@@ -15,7 +15,7 @@ Template.maps.helpers({
     if (GoogleMaps.loaded()) {
       return {
         center: new google.maps.LatLng(findClientLatitude(), findClientLongitude()),
-        zoom: 17
+        zoom: 7
       };
     }
   }
@@ -23,15 +23,23 @@ Template.maps.helpers({
 
 
 Template.maps.onCreated(function() {
-  GoogleMaps.ready('exampleMap', function(map) {
-    console.log(map)
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
+
+Meteor.call('getBikeData', function(err,res){
+
+    GoogleMaps.ready('exampleMap', function(map) {
+      var myLatLng = new google.maps.LatLng(res.latitude, res.longitude);
+      var marker = new google.maps.Marker({
+        position: map.options.center,
+        map: map.instance
+      });
+      var bikeRackMarkers = new google.maps.Marker({
+        position: myLatLng,
+        map: map.instance
+      });
     });
   });
 });
 
-Meteor.call('getBikeData', function(err,res){
-  console.log(res);
-});
+
+
+
--- pocketsf/lib/bikeApi.js
+++ pocketsf/lib/bikeApi.js
@@ -7,7 +7,20 @@ if (Meteor.isServer) {
       var apiUrl = 'https://data.sfgov.org/resource/w969-5mn4.json';
       // query the API
       var response = HTTP.get(apiUrl).data;
-      return response;
+      // return response;
+      var bikeMarker = {};
+
+      var responseLat = response[0].latitude.latitude;
+      var responseLong = response[0].latitude.longitude;
+      var responseName = response[0].street_name;
+      var responseSpaces = response[0].spaces;
+      bikeMarker.latitude = responseLat;
+      bikeMarker.longitude = responseLong;
+      bikeMarker.name = responseName;
+      bikeMarker.spaces = responseSpaces;
+
+      return bikeMarker;
     }
+
   });
 }
