523e0f86ca6eed86a4d91bf95bdc6f1478f2f7ef
Max McGee <mmcgeevibes@gmail.com>
2015-11-04 11:17:50 -0800

    add setting.json test

--- client/templates/food_scores_maps.js
+++ client/templates/food_scores_maps.js
@@ -43,11 +43,12 @@ Template.foodScoresMaps.onCreated(function(){
       }
 
       var LatLng = new google.maps.LatLng(marker.latitude, marker.longitude);
+
       var foodScoreMarker = new google.maps.Marker({
         position: LatLng,
         map: map.instance,
         icon: icon,
-        content: "<strong>Name:</strong> " + marker.name + "<br><strong>Address:</strong> " + marker.address + "<br><strong>Risk Level:</strong> " + marker.risk_category + "<br><strong>Score:</strong> " + marker.insp_score + "<br><strong>Inspection Results:</strong> " + marker.insp_description
+        content: "<strong>Name:</strong> " + marker.name + "<br><strong>Address:</strong> " + marker.address + "<br><strong>Risk Level:</strong> " + marker.risk_category + "<br><strong>Score:</strong> " + marker.insp_score + "<br><strong>Inspection Results:</strong> " + marker.insp_description.slice(0, 2)
       });
 
       var infowindow = null;
@@ -61,58 +62,8 @@ Template.foodScoresMaps.onCreated(function(){
     });
     markers = {}
 
-
-    // for (var i = 0; i < foodScoreMarkers.length; i++) {
-    //   // add click listeners to all markers
-    //   console.log(foodScoreMarkers[1])
-    //   var marker = foodScoreMarkers[i];
-
-    //   google.maps.event.addListener(marker, 'click', function() {
-    //     infowindow.setContent(this.content);
-    //     infowindow.open(map.instance, this);
-    //   });
-    // };
-
   });
 });
-
-      // var foodScoreRecords = FoodScores.find().count()
-
-
-
-        // for (var i = 0 ; i < foodScoreRecords;  i++) {
-        //   var marker = res[i]
-
-        //   var LatLng = new google.maps.LatLng(marker.latitude, marker.longitude)
-
-        //   var mobileFoodMarker = new google.maps.Marker({
-        //     position: LatLng,
-        //     map: map.instance,
-        //     content: "Vendor: " + marker.vendor + " <br> Info: " + marker.info + "<br> Address: " + "<a href='http://maps.google.com/?q=" + marker.address + "'>" + marker.address + "</a><br>Hours: " + marker.startTime + "- " + marker.endTime + "<br>Day(s) Open: " + marker.dayOfWeek + "<br><a href='https://www.google.com/maps/dir/" + Geolocation.currentLocation().coords.latitude + "," + Geolocation.currentLocation().coords.longitude + "/" + marker.address + "'>get directions</a>"
-        //   });
-
-        //   var infowindow = null;
-        //   infowindow = new google.maps.InfoWindow({
-        //   content: "loading..."
-        //   })
-
-        //   markers.push(mobileFoodMarker);
-        // };
-        // for (var i = 0; i < markers.length; i++) {
-        //   // add click listeners to all markers
-        //   var marker = markers[i]
-
-        //   google.maps.event.addListener(marker, 'click', function() {
-        //     infowindow.setContent(this.content);
-        //     infowindow.open(map.instance, this);
-        //   });
-        // };
-
-        // var userMarker = new google.maps.Marker({
-        // position: map.options.center,
-        // map: map.instance,
-        // icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-        // });
 
 
 
--- client/templates/mobile_foods.js
+++ client/templates/mobile_foods.js
@@ -1,7 +1,9 @@
 Template.mobileFoods.events({
   "submit .address": function(e){
+    console.log('hi')
     e.preventDefault();
     var text = e.target.destination.value;
+    console.log(text)
     Meteor.call('findGeoCode', text, function(err, res) {
       Session.set("userLat", res[0].latitude)
       Session.set("userLng", res[0].longitude)
