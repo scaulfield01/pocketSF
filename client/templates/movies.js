7df037f865432351198921f2f018ca90bc742f0a
scaulfield01 <s.caulfield88@gmail.com>
2015-11-03 16:00:58 -0800

    remove original solution

--- client/templates/movies.js
+++ client/templates/movies.js
@@ -2,10 +2,9 @@ Template.moviesMap.onRendered(function() {
   GoogleMaps.load();
 });
 
-Template.moviesMap.helpers({
 
+Template.moviesMap.helpers({
   moviesMapOptions: function() {
-    // Make sure the maps API has loaded
     var findClientLatitude = function() {
     return Geolocation.currentLocation().coords.latitude};
     var findClientLongitude = function() {
@@ -20,61 +19,30 @@ Template.moviesMap.helpers({
   }
 });
 
-
 Template.moviesMap.onCreated(function() {
 
-  GoogleMaps.ready('moviesMap', function(map) {
+  Meteor.call('getMoviesData', function(err,res){
+   debugger
+    GoogleMaps.ready('moviesMap', function(map) {
 
-
-
-
-    Meteor.call('getMoviesData', function(err, res){
       var markers = []
       for (var i = 0 ; i <  res.length ;  i++) {
         var marker = res[i]
-
-        Meteor.call('findGeoCode', marker.location, function(error, result){
-        debugger
-        if(error){
-            console.log(error);
-        } else {
-            console.log(result);
-        }
-    });
-
-
-
-        var moviesMarker = new google.maps.Marker({
+        // var LatLng = new google.maps.LatLng(marker.latitude, marker.longitude)
+        var movieMarker = new google.maps.Marker({
           position: LatLng,
-          map: map.instance
+          map: map.instance,
         });
 
-        var infowindow = null;
-        infowindow = new google.maps.InfoWindow({
-        content: "loading..."
-        })
-
-        markers.push(moviesMarker)
-      };
-
-      for (var i = 0; i < markers.length; i++) {
-        // add click listeners to all markers
-        var marker = markers[i]
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(this.content);
-          infowindow.open(map.instance, this);
-        });
-      };
-
-      // create marker at client's location
       var userMarker = new google.maps.Marker({
       position: map.options.center,
       map: map.instance,
       icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
       });
 
-    });
+    };
   });
 });
+});
+
 
